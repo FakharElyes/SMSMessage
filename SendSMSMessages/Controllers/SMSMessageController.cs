9f7bb241ccfa25e86dc7754c977edbcb92b77626
Elyes FAKHAR <elyes.fakhar.ext@trendit.fr>
2021-08-31 11:50:36 +0100

    send message

---
+++ SendSMSMessages/Controllers/SMSMessageController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexmo.Api;
+using SendSMSMessages.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SendSMSMessages.Controllers
+{
+    public class SMSMessageController : Controller
+    {
+
+        string TutoLink = "https://www.c-sharpcorner.com/article/asp-net-mvc-sending-sms-messages-using-nexmo-api/";
+
+
+        // GET: SMSMessage  
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(Message message)
+        {
+            var results = SMS.Send(new SMS.SMSRequest
+            {
+                from = Configuration.Instance.Settings["appsettings:NEXMO_FROM_NUMBER"],
+                to = message.To,
+                text = message.ContentMsg
+            });
+
+
+            return View();
+        }
+    }
+}
---
+++ SendSMSMessages/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SendSMSMessages.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
